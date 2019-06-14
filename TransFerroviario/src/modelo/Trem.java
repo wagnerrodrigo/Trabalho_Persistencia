67ff338a6c92c11224ba02de453757e9238ad1bc
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2019-06-14 19:33:07 -0300

    classe Trem

--- TransFerroviario/src/modelo/Trem.java
+++ TransFerroviario/src/modelo/Trem.java
@@ -10,5 +10,53 @@ package modelo;
  * @author wagner
  */
 public class Trem {
+    public String apelido;
+    public int maxVagao;
+    public int nunVagao;
+    public float pesoTotalVagao;
+
+    public Trem(String apelido, int maxVagao, int nunVagao, float pesoTotalVagao) {
+        this.apelido = apelido;
+        this.maxVagao = maxVagao;
+        this.nunVagao = nunVagao;
+        this.pesoTotalVagao = pesoTotalVagao;
+    }
+
+    public Trem() {
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public int getMaxVagao() {
+        return maxVagao;
+    }
+
+    public void setMaxVagao(int maxVagao) {
+        this.maxVagao = maxVagao;
+    }
+
+    public int getNunVagao() {
+        return nunVagao;
+    }
+
+    public void setNunVagao(int nunVagao) {
+        this.nunVagao = nunVagao;
+    }
+
+    public float getPesoTotalVagao() {
+        return pesoTotalVagao;
+    }
+
+    public void setPesoTotalVagao(float pesoTotalVagao) {
+        this.pesoTotalVagao = pesoTotalVagao;
+    }
+    
+    
     
 }
