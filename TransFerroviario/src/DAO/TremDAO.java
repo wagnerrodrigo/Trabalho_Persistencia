bc60170652c5cba0c99b9db1590e1ec2d08af3d8
wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-06-27 15:42:14 -0300

    metodos de salvar e excluir feitos

--- TransFerroviario/src/DAO/TremDAO.java
+++ TransFerroviario/src/DAO/TremDAO.java
@@ -4,11 +4,33 @@
  * and open the template in the editor.
  */
 package DAO;
-
+import javax.persistence.EntityManager;
+import modelo.Trem;
 /**
  *
  * @author wagner
  */
 public class TremDAO {
+    
+    public void salvaTrem(Trem trem){
+        EntityManager em = Conexao.getEntityManager();
+                     em.getTransaction().begin();
+       if (trem.getNumero() != 0){
+           trem = em.merge(trem);
+       }
+                     em.persist(trem);
+                     em.getTransaction().commit();
+                     em.close();
+    }
+    
+    
+     public void excluirTrem (Trem trem){
+       EntityManager em = Conexao.getEntityManager();
+                     em.getTransaction().begin();
+                     trem = em.merge(trem);
+                     em.remove(trem);
+                     em.getTransaction().commit();
+                     em.close();   
+   }
     
 }
--- TransFerroviario/src/DAO/VagaoDAO.java
+++ TransFerroviario/src/DAO/VagaoDAO.java
@@ -5,10 +5,32 @@
  */
 package DAO;
 
+import javax.persistence.EntityManager;
+import modelo.Vagao;
+
 /**
  *
  * @author wagner
  */
 public class VagaoDAO {
+     public void salvaVagao(Vagao vagao){
+        EntityManager em = Conexao.getEntityManager();
+                     em.getTransaction().begin();
+       if (vagao.getCodigo() != 0){
+           vagao = em.merge(vagao);
+       }
+                     em.persist(vagao);
+                     em.getTransaction().commit();
+                     em.close();
+    }
     
+    
+     public void excluirVagao (Vagao vagao){
+       EntityManager em = Conexao.getEntityManager();
+                     em.getTransaction().begin();
+                     vagao = em.merge(vagao);
+                     em.remove(vagao);
+                     em.getTransaction().commit();
+                     em.close();   
+   }
 }
